8b5356df4f3735421a7930dd58b2e46c09a9515f
Mikko Moisio <mmoisio@kiisseli.com>
2020-05-29 14:33:49 +0300

    add cli arg parsing

--- scraper.py
+++ scraper.py
@@ -7,68 +7,93 @@ import time
 import datetime
 from datetime import date
 import inspect
+import sys
 
 class TweetEncoder(json.JSONEncoder):
     def default(self, obj):
-         if isinstance(obj, Tweet):
-             return [obj.id, obj.permalink, obj.username, obj.to, obj.text, str(obj.date), str(obj.retweets), str(obj.favorites), str(obj.mentions), obj.hashtags, obj.geo]
-         return json.JSONEncoder.default(self, obj)
+        if isinstance(obj, Tweet):
+            return [obj.id, obj.permalink, obj.username, obj.to, obj.text, str(obj.date), str(obj.retweets), str(obj.favorites), str(obj.mentions), obj.hashtags, obj.geo]
+        return json.JSONEncoder.default(self, obj)
 
-path_raw = './data/raw/%s.txt'
 
-usernames = ['realDonaldTrump']
 
-beginning_of_time = date(2006, 3, 21)
-ttimer = time.time()
-for username in usernames:
-    print('* Processing user "' +  username + '".')
-
-    sleep_time = 5
-    time_slice = 30
-
-    end = date.today() + datetime.timedelta(days=1) # the upper bound in exclusive so set it to be tomorrow
+def scrape(save_path, usernames):
+    beginning_of_time = date(2006, 3, 21)
     
-    while True:
-        start = end - datetime.timedelta(days=time_slice)
 
-        print('** Querying Twitter... (user = ' + username + ', interval = ' + start.isoformat() + ' - ' + end.isoformat() + ', slice = ' + str(time_slice)+ 'd)')
-        try:
-            timer = time.time()
+    for username in usernames:
+        print('* Processing user "' + username + '".')
+        ttimer = time.time()
+        sleep_time = 5
+        time_slice = 30
 
-            tweetCriteria = got.manager.TweetCriteria().setUsername(username).setSince(start.isoformat()).setUntil(end.isoformat())
-            tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+        # the upper bound in exclusive so set it to be tomorrow
+        end = date.today() + datetime.timedelta(days=1)
 
-            tweet_count = len(tweets)
-            elapsed = time.time() - timer
+        while True:
+            start = end - datetime.timedelta(days=time_slice)
 
-            print('>> Query finished (number of tweets = ' + str(tweet_count) + ', elapsed time = {:.2f}'.format(elapsed) + 's).')
+            print('** Querying Twitter... (user = ' + username + ', interval = ' +
+                  start.isoformat() + ' - ' + end.isoformat() + ', slice = ' + str(time_slice) + 'd)')
+            try:
+                timer = time.time()
 
-            with open(path_raw % username, mode='a') as f:
-                for tweet in tweets:
-                    f.write(json.dumps(tweet, cls=TweetEncoder, ensure_ascii=False))
-                    f.write('\n')
+                tweetCriteria = got.manager.TweetCriteria().setUsername(
+                    username).setSince(start.isoformat()).setUntil(end.isoformat())
+                tweets = got.manager.TweetManager.getTweets(tweetCriteria)
 
-            time_slice *= 2
-            sleep_time = max(1, sleep_time // 2)
+                tweet_count = len(tweets)
+                elapsed = time.time() - timer
 
-            end = start
+                print('>> Query finished (number of tweets = ' + str(tweet_count) +
+                      ', elapsed time = {:.2f}'.format(elapsed) + 's).')
 
-            if end < beginning_of_time:
-                print('Scraping finished in {:.2f}'.format(time.time() -  ttimer) + 's.')
-                break
+                with open(path_raw % username, mode='a') as f:
+                    for tweet in tweets:
+                        f.write(json.dumps(
+                            tweet, cls=TweetEncoder, ensure_ascii=False))
+                        f.write('\n')
 
-        except BaseException as ex:
+                time_slice *= 2
+                sleep_time = max(1, sleep_time // 2)
 
-            # A hacky way to check wether the exception was raised by a failing HTTP request
-            if (inspect.trace()[-1][2] == 348 and inspect.trace()[-1][3] == 'getJsonResponse'):
-                time_slice = max(1, time_slice // 2)
-                sleep_time *= 2
-            else:
-                print('Something went wrong:', str(ex))
-                import sys
-                sys.exit()
-            
-            
-            
-        print('^^ Sleeping... (sleep_time = ' + str(sleep_time) + 's, time_slice = ' + str(time_slice) + 'd).')
-        time.sleep(sleep_time)
+                end = start
+
+                if end < beginning_of_time:
+                    print('Scraping finished in {:.2f}'.format(
+                        time.time() - ttimer) + 's for user ' + username + '.')
+                    break
+
+            except BaseException as ex:
+
+                # A hacky way to check wether the exception was raised by a failing HTTP request
+                if (inspect.trace()[-1][2] == 348 and inspect.trace()[-1][3] == 'getJsonResponse'):
+                    time_slice = max(1, time_slice // 2)
+                    sleep_time *= 2
+                else:
+                    print('Something went wrong:', str(ex))
+                    sys.exit()
+
+            print('^^ Sleeping... (sleep_time = ' + str(sleep_time) +
+                  's, time_slice = ' + str(time_slice) + 'd).')
+            time.sleep(sleep_time)
+
+
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', help='set a path for saving results, overrides default (./data/raw/)')
+    parser.add_argument('-u', '--users', help='supply a list of users to query', nargs='*')
+    args = parser.parse_args()
+
+    path_raw = './data/raw/%s.txt'
+    usernames = ['realDonaldTrump', 'MikeBloomberg']
+    if args.path:
+        path_raw = args.path + '%s.txt'
+    if args.users:
+        usernames = args.users
+
+    scrape(path_raw, usernames)
